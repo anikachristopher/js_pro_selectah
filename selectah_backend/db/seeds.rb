91f4563041c2c697ad4f4b072025198e94bacb91
anikachristopher <anika.mchristoper@gmail.com>
2020-08-09 21:08:07 -0400

    seeded database using Faker gem

--- selectah_backend/db/seeds.rb
+++ selectah_backend/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+75.times do
+    Song.create ([{
+        name: Faker::Music::Prince.song,
+        artist: Faker::Music.band,
+        genre: Faker::Music.genre
+    }])
+end
+
+# Song.create(name: "anikac", artist: "anika", genre: "pop")
+
+# Playlist.create(title: "car", description: "driving to work", date: 1/2/20)
+
