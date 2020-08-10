fc2211d7d2dd55b7363b5f41914c9c77d2aa34ab
anikachristopher <anika.mchristoper@gmail.com>
2020-08-09 21:25:58 -0400

    created DOM

---
+++ selectah_frontend/index.js
@@ -0,0 +1,15 @@
+testing
+1234
+
+testing 
+
+Project structure 
+Selectah - Music Player
+
+Models 
+Playlist- playlist_name; description; date_created
+Songs - song_name; artist_name; genre; 
+
+Associations
+playlist has_many songs
+songs belong_to playlist
