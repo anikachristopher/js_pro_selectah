24db8d75ec198acc388287b360c957812315c5f7
anikachristopher <anika.mchristoper@gmail.com>
2020-08-16 17:17:03 -0400

    re-seeded database

---
+++ selectah_frontend/src/index.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetchSongs()
+
+})
+
+    const BASE_URL = "http://127.0.0.1:3000"
+
+    function fetchSongs(){
+        fetch(`${BASE_URL}/songs`)
+        // .then(fetchSongs (response){
+        //     return response.json();}
+        // .then(fetchSongs(json){
+    })
