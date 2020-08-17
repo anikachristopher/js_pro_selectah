45e8b38c9ef36a767c760d7a006a91319e1469e4
anikachristopher <anika.mchristoper@gmail.com>
2020-08-16 21:23:44 -0400

    updated seed info

--- selectah_backend/db/seeds.rb
+++ selectah_backend/db/seeds.rb
@@ -17,8 +17,19 @@
 # end
 
 
-Song.create(name: 'Fake Love', artist: 'Duncan Mighty', genre: 'afro beat')
-Song.create(name: 'Ye', artist: 'Burna Boy', genre: 'afro beat')
-Song.create(name: 'Monsterz Out', artist: 'K Lion', genre: 'trinibad')
-Song.create(name: 'Go Crazy', artist: 'Chris Brown', genre: 'pop')
-Song.create(name: 'Must Be', artist: 'J Hus', genre: 'grime')
+one = Playlist.create(title: "Ready for Reggae", description: "Old School Reggae Music", date_created: "07-01-20")
+two = Playlist.create(title: "Pop Playground", description: "All the Newest Pop Music", date_created: "07-15-20")
+three = Playlist.create(title: "Afro Beat Party", description: "Most Popluar Afro Beat Music", date_created: "07-30-20")
+four = Playlist.create(title: "Trinibad", description: "Trinidad Dancehall Hits", date_created: "08-01-20")
+five = Playlist.create(title: "Grime Time", description: "London Grime Music", date_created: "08-10-20")
+six = Playlist.create(title: "Rap Attack", description: "New Rap Hits", date_created: "08-15-20")
+
+
+# Song.create(name: 'Fake Love', artist: 'Duncan Mighty', genre: 'afro beat', playlist: three)
+first = Song.create(name: 'Ye', artist: 'Burna Boy', genre: 'afro beat', playlist: three)
+second = Song.create(name: 'Monsterz Out', artist: 'K Lion', genre: 'trinibad', playlist: four)
+third = Song.create(name: 'Go Crazy', artist: 'Chris Brown', genre: 'pop', playlist: two)
+fourth = Song.create(name: 'Must Be', artist: 'J Hus', genre: 'grime', playlist: five)
+fifth = Song.create(name: 'Three Little Birds', artist: 'Bob Marley', genre: 'reggae', playlist: one)
+sixth = Song.create(name: 'Highest in The Room', artist: 'Travis Scott', genre: 'rap', playlist: six)
+
