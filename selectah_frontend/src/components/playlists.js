4660a5f0db71a87cb14f4888ecbe95d1ef4d52c5
anikachristopher <anika.mchristoper@gmail.com>
2020-08-24 21:42:04 -0400

    made fetch request to load playlists

--- selectah_frontend/src/components/playlists.js
+++ selectah_frontend/src/components/playlists.js
@@ -6,7 +6,22 @@ class Playlists{
         this.fetchAndLoadPlaylists()
     }
 
-    fetchAndLoadPlaylists() {
-        this.adapter.getPlaylists().then(playlists => {console.log(playlists)})
+
+    fetchAndLoadPlaylists(){
+        this.adapter.getPlaylists().then(playlists =>{console.log(playlists)})
     }
+
+    // fetchAndLoadPlaylists() {
+    //     const allplaylists = []
+    //     this.playlistsAdapter.getPlaylists()
+    //     .then(playlists => {
+    //         playlists.forEach(playlist => allplaylists.push(new Playlist(playlist.id, playlist.attributes.text, playlist.attributes.post_id)))
+    //         console.log(allplaylists)
+    //     })
+
+
+
+    // fetchAndLoadPlaylists() {
+    //     this.adapter.getPlaylists().then(playlists => {console.log(playlists)})
+    // }
 }
