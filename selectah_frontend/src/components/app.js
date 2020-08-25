7879c6ec1247f288ee20842e89148f121b21ccf9
anikachristopher <anika.mchristoper@gmail.com>
2020-08-24 21:41:35 -0400

    created App class

--- selectah_frontend/src/components/app.js
+++ selectah_frontend/src/components/app.js
@@ -1,8 +1,19 @@
 class App{
-    constructor(){
+    constructor() {
         this.playlists = new Playlists()
     }
 }
 
+
+
+//this instatiates a new instance of the Playlists class
+
+// class App {
+//     start(){
+//     const playlists = new Playlists()
+//     playlists.fetchAndLoadSongs()
+//     }
+// }
+
 // instantiates a new instance of my playlist class
-// creating a new instance of app kicks off the constructor which willcreate a new instance of the playlist class
+// creating a new instance of app kicks off the constructor which will create a new instance of the playlist class
